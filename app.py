e30bdaa285929c7c07da1b67ccf2958c27666bcd
Khalil Ur Rehman <95528236+Khalil-Rehman9@users.noreply.github.com>
2023-12-01 01:59:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-import openai
+from openai import OpenAI
 import streamlit as st
 from youtube_transcript_api import YouTubeTranscriptApi
 from gtts import gTTS 
 import io
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Retrieve the OpenAI API key from the environment
-openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key= "YOUR_API_KEY")
 
 st.set_page_config(page_title="Video Summarizer", page_icon=":movie_camera:")
 
@@ -21,19 +17,22 @@ def get_transcript(video_url):
     video_id = video_url.split("v=")[-1]
     transcript = YouTubeTranscriptApi.get_transcript(video_id)
     text = ' '.join([line['text'] for line in transcript]) 
+    
     return text
+    
+
+
 
 @st.cache_resource
 def summarize(transcript):
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt = f"Generate a concise summary of the following content in approximately 400 words: \n\n{transcript}",
-        max_tokens=400,
-        n=1,
-        stop=None,
-        temperature=0.5,
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Give me 400 Word Summery of the Following Text"},
+            {"role": "user", "content": transcript} ,
+        ]
     )
-    summary = response.choices[0].text.strip()
+    summary = response.choices[0].message.content.strip()
     return summary
 
 @st.cache_resource
